5c14244d025da74553f2aa08e14c3ea55da5091d
jr6666 <janakiram_b18@vnrvjiet.in>
2020-08-04 19:46:13 +0530

    Updates

---
+++ precache-manifest.752036890f8921c11613b6ab23f0d925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abf6b55f460e64e884449dafe8258ab",
+    "url": "/examplereactapp/index.html"
+  },
+  {
+    "revision": "32d2b8b930bc4d67ac2f",
+    "url": "/examplereactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aefb0b93dc92cc7af8b3",
+    "url": "/examplereactapp/static/js/2.e8c9e4ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/examplereactapp/static/js/2.e8c9e4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d2b8b930bc4d67ac2f",
+    "url": "/examplereactapp/static/js/main.533dbc16.chunk.js"
+  },
+  {
+    "revision": "4828c713cc885fa7be28",
+    "url": "/examplereactapp/static/js/runtime-main.9bbd8401.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/examplereactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
